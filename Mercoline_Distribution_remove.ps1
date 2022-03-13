2a5197f42f0349b14a5cfc739496167c2b4cd0a8
jahube <70081790+jahube@users.noreply.github.com>
2022-03-13 11:25:59 +0100

    Create Mercoline_Distribution_remove.ps1

---
+++ Mercoline_Distribution_remove.ps1
@@ -0,0 +1,22 @@
+
+$Domain_before = "meroline.de"
+
+$Domain_after = "mercoline-mcl.de"
+
+$groups = get-distributiongroup -ResultSize unlimited -organizationalunit "OU=_Mercoline_Verteilerlisten,DC=mercoline,DC=local"
+
+$groups |ft primarysmtpaddress,EmailAddresses,windowsemailaddress
+
+foreach ($group in $groups) {
+
+Set-DistributionGroup $group.distinguishedname -EmailAddressPolicyEnabled:$false
+
+$SMTP =$group.PrimarySmtpAddress
+
+$NEW_SMTP = $SMTP -replace ( $Domain_before, $Domain_after )
+
+Set-DistributionGroup $group.distinguishedname -PrimarySmtpAddress $NEW_SMTP -Confirm:$false -force
+
+Set-DistributionGroup $group.distinguishedname -WindowsEmailAddress $NEW_SMTP -Confirm:$false -force
+
+}
